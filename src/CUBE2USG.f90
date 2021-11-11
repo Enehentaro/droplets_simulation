7cfe69d1883aa21f7a80eafe04103573f66fb35e
mochimochikun <konishihikarukit@gmail.com>
2021-11-11 12:09:32 +0900

    ファイルを複数選択して変換できるようにしました”
    ファイルを複数選択して変換できるようにしました”

--- src/CUBE2USG.f90
+++ src/CUBE2USG.f90
@@ -7,32 +7,45 @@ program main
     use vtkMesh_operator_m
     implicit none
     character(50) F_fname, USG_fname
-    integer i, CUBEID, nodeID(3), n, num_node
+    character(50), allocatable :: fname(:)
+    integer i, j, CUBEID, nodeID(3), n, num_node, num_file
     real X(3)
 
-    print *, 'F_FileName ?'
-    read(5,*) F_fname
+    print *, 'num_file ?'
+    read(5,*) num_file
 
+    allocate(fname(num_file))
+    
+    print *, 'F_FileName ?'
+    do i = 1, num_file
+        read(5,*) fname(i)
+    end do
+    
     print *, 'UnstructuredGRID_FileName ?'
     read(5,*) USG_fname
 
-    call read_CUBE_data(F_fname, '')
+    do j = 1, num_file
+        F_fname = fname(j)
 
-    call read_VTK_mesh(USG_fname, meshONLY=.true.)
+        call read_CUBE_data(F_fname, '')
 
-    do i = 0, size(cell_array)-1
-        X(:) = 0.0
-        num_node = size(cell_array(i)%nodeID)
-        do n = 1, num_node
-            X(:) = X(:) + node_array(cell_array(i)%nodeID(n))%coordinate(:)
+        call read_VTK_mesh(USG_fname, meshONLY=.true.)
+    
+        do i = 0, size(cell_array)-1
+            X(:) = 0.0
+            num_node = size(cell_array(i)%nodeID)
+            do n = 1, num_node
+                X(:) = X(:) + node_array(cell_array(i)%nodeID(n))%coordinate(:)
+            end do
+            X(:) = X(:) / real(num_node)
+            CUBEID = get_cube_contains(X)    
+            nodeID(:) = nearest_node(X, CUBEID)
+            cell_array(i)%vector(:) = get_velocity_f(nodeID, CUBEID)
         end do
-        X(:) = X(:) / real(num_node)
-        CUBEID = get_cube_contains(X)    
-        nodeID(:) = nearest_node(X, CUBEID)
-        cell_array(i)%vector(:) = get_velocity_f(nodeID, CUBEID)
+    
+        i = len_trim(F_fname)
+        call output_VTK_mesh(F_fname(:i-2)//'.vtk')
+    
     end do
-
-    i = len_trim(F_fname)
-    call output_VTK_mesh(F_fname(:i-2)//'.vtk')
     
 end program main
