ac93d7d3914f49744eb762a3b0d7b869ccb80506
KishiShohei <sk2.090412066@gmail.com>
2021-11-12 13:32:11 +0900

    modify

--- mingw-w64.bat
+++
@@ -1,6 +0,0 @@
-echo off
-set PATH=C:\Program Files (x86)\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin;%PATH%
-rem echo %PATH%
-rem cd "C:\Program Files (x86)\mingw-w64\i686-8.1.0-posix-dwarf-rt_v6-rev0\mingw32\bin"
-
-"C:\WINDOWS\system32\cmd.exe"
