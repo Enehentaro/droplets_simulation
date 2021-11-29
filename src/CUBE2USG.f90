811be01e50fce58221a7e91cc02a926568c705c6
mochimochikun <konishihikarukit@gmail.com>
2021-11-29 18:06:55 +0900

    txtファイルを読み込めるように改良中

--- src/CUBE2USG.f90
+++ src/CUBE2USG.f90
@@ -1,58 +1,39 @@
 program CUBE2USG
     use CUBE_mod
     use vtkMesh_operator_m
+	use counter
     implicit none
     character(50) F_fname, USG_fname
-    character(50), allocatable :: fname(:)
-    integer i, j, n, num_node, num_file, num_cell
+    character(50), allocatable :: field_name(:)
+    integer i, j, CUBEID, nodeID(3), n, num_node, num_file
     real X(3)
 
-    type nodeInfo
-        integer cubeID, nodeID(3)
-    end type
+    num_file=count_txt()
+    print *, num_file
 
-    type(nodeInfo), allocatable :: vtkCell2cubeNode(:)
-
-    print *, 'num_file ?'
-    read(5,*) num_file
-
-    allocate(fname(num_file))
-    
-    print *, 'F_FileName ?'
-    do i = 1, num_file
-        read(5,*) fname(i)
-    end do
+    allocate(field_name(num_file))
+    call read_txt(num_file,field_name)
     
     print *, 'UnstructuredGRID_FileName ?'
     read(5,*) USG_fname
 
     call read_VTK_mesh(USG_fname, meshONLY=.true.)
 
-    num_cell = size(cell_array)
-
     do j = 1, num_file
-        F_fname = fname(j)
+        F_fname = field_name(j)
 
         call read_CUBE_data(F_fname, '')
     
-        if (.not.allocated(vtkCell2cubeNode)) then
-            allocate(vtkCell2cubeNode(0 : num_cell - 1))
-
-            do i = 0, num_cell-1
-                X(:) = 0.0
-                num_node = size(cell_array(i)%nodeID)
-                do n = 1, num_node
-                    X(:) = X(:) + node_array(cell_array(i)%nodeID(n))%coordinate(:)
-                end do
-                X(:) = X(:) / real(num_node)
-                vtkCell2cubeNode(i)%cubeID = get_cube_contains(X)    
-                vtkCell2cubeNode(i)%nodeID(:) = nearest_node(X, vtkCell2cubeNode(i)%cubeID)
+        do i = 0, size(cell_array)-1
+            X(:) = 0.0
+            num_node = size(cell_array(i)%nodeID)
+            do n = 1, num_node
+                X(:) = X(:) + node_array(cell_array(i)%nodeID(n))%coordinate(:)
             end do
-
-        end if
-
-        do i = 0, num_cell-1
-            cell_array(i)%vector(:) = get_velocity_f(vtkCell2cubeNode(i)%nodeID(:), vtkCell2cubeNode(i)%cubeID)
+            X(:) = X(:) / real(num_node)
+            CUBEID = get_cube_contains(X)    
+            nodeID(:) = nearest_node(X, CUBEID)
+            cell_array(i)%vector(:) = get_velocity_f(nodeID, CUBEID)
         end do
     
         i = len_trim(F_fname)
