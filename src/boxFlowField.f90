de13006cc8920291c946626bfa0939b164aec92f
Tauzu <tauzushuka@gmail.com>
2022-05-24 10:54:32 +0900

    boxFlowField update

--- src/boxFlowField.f90
+++ src/boxFlowField.f90
@@ -1,21 +1,97 @@
-program boxFlowField
-    use conditionValue_m
+module boxOperator_m
     use boxCounter_m
-    use unstructuredGrid_mod
-    use terminalControler_m
     implicit none
-    integer i_box, num_box, nc
-    character(255) caseName
-    type(boxCounter), allocatable :: box_array(:)
 
-    type boxResult_t
+    private
+
+    type(boxCounter), allocatable, public :: box_array(:)
+
+    type, public :: boxResult_t
         real flowVelocity(3)
     end type
-    type(boxResult_t), allocatable :: bResult(:)
 
+    public setup_boxArray, output_boxVTK, output_flowCSV
+
+    contains
+
+    subroutine setup_boxArray
+        box_array = get_box_array('.', 0)
+    end subroutine
+
+    subroutine output_flowCSV(fname, bResult)
+        character(*), intent(in) :: fname
+        type(boxResult_t), intent(in) :: bResult(:)
+        integer n_unit, i
+
+        print*, 'output: ', fname
+
+        open(newunit=n_unit, file=fname, status='replace')
+        
+            write(n_unit, '("x,y,z,u,v,w")')
+            
+            do i = 1, size(box_array)
+                write(n_unit,'(*(g0:,","))') box_array(i)%center, bResult(i)%flowVelocity
+            end do
+
+        close(n_unit)
+
+    end subroutine
+
+    subroutine output_boxVTK(fname, bResult)
+        use vtkMesh_operator_m
+        character(*), intent(in) :: fname
+        type(boxResult_t), intent(in) :: bResult(:)
+        type(vtkMesh) boxMesh
+        integer i, j, k, num_box
+        real, parameter :: trans(3,8) = reshape([ &
+                                            0.0,0.0,0.0, 1.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0, &
+                                            0.0,0.0,1.0, 1.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0], shape(trans))
+        real velArray(3, size(box_array))
+        real, allocatable :: xyz(:,:)
+        integer, allocatable :: vertices(:,:), types(:)
+                                        
+        num_box = size(box_array)
+        allocate(xyz(3, num_box*8))
+        allocate(vertices(8, num_box), types(num_box))
+        do i = 1, num_box
+
+            do j = 1, 8
+                k = j + 8*(i-1)
+                xyz(:,k) = box_array(i)%min_cdn(:) + box_array(i)%width(:)*trans(:,j)
+                vertices(j,i) = k
+            end do
+            
+            types(i) = 11
+
+        end do
+
+        do i = 1, num_box
+
+            velArray(:,i) = bResult(i)%flowVelocity
+
+        end do
+
+        boxMesh = vtkMesh_(xyz, vertices, types)
+
+        call boxMesh%output(fname, cellVector=velArray, vectorName='VEL')
+
+    end subroutine
+
+end module boxOperator_m
+
+program boxFlowField
+    use conditionValue_m
+    use unstructuredGrid_mod
+    use terminalControler_m
+    use boxOperator_m
+    implicit none
+    integer i_box, nc, num_box
+    character(255) caseName
+    type(boxResult_t), allocatable :: bResult(:)
     type(UnstructuredGrid) mesh
 
-    box_array = get_box_array('.', 0)
+    call setup_boxArray
+
     num_box = size(box_array)
 
     do nc = 1, iargc()
@@ -46,69 +122,11 @@ program boxFlowField
 
         end block
     
-        call output_countCSV
-        call output_boxVTK
+        call output_flowCSV(trim(caseName)//'/BoxFlow.csv', bResult)
+        call output_boxVTK(trim(caseName)//'/BoxFlow.vtk', bResult)
 
         deallocate(bResult)
 
     end do
-
-    contains
-
-    subroutine output_countCSV
-        integer n_unit, i
-        character(:), allocatable :: csvFName
-
-        csvFName = trim(caseName)//'/BoxFlow.csv'
-        print*, 'output: ', csvFName
-
-        open(newunit=n_unit, file=csvFName, status='replace')
-        
-            write(n_unit, '("x,y,z,u,v,w")')
-            
-            do i = 1, size(box_array)
-                write(n_unit,'(*(g0:,","))') box_array(i)%center, bResult(i)%flowVelocity
-            end do
-
-        close(n_unit)
-
-    end subroutine
-
-    subroutine output_boxVTK
-        use vtkMesh_operator_m
-        type(vtkMesh) boxMesh
-        integer i, j, k
-        real, parameter :: trans(3,8) = reshape([ &
-                                            0.0,0.0,0.0, 1.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0, &
-                                            0.0,0.0,1.0, 1.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0], shape(trans))
-        real velArray(3, size(box_array))
-        real, allocatable :: xyz(:,:)
-        integer, allocatable :: vertices(:,:), types(:)
-                                        
-        allocate(xyz(3, num_box*8))
-        allocate(vertices(8, num_box), types(num_box))
-        do i = 1, num_box
-
-            do j = 1, 8
-                k = j + 8*(i-1)
-                xyz(:,k) = box_array(i)%min_cdn(:) + box_array(i)%width(:)*trans(:,j)
-                vertices(j,i) = k
-            end do
-            
-            types(i) = 11
-
-        end do
-
-        do i = 1, num_box
-
-            velArray(:,i) = bResult(i)%flowVelocity
-
-        end do
-
-        boxMesh = vtkMesh_(xyz, vertices, types)
-
-        call boxMesh%output(trim(caseName)//'/BoxFlow.vtk', cellVector=velArray, vectorName='VEL')
-
-    end subroutine
 
 end program boxFlowField
