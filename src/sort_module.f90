47ce265ca0cb752cfc0aea1ec23e13905dc2d1f8
mochimochikun <konishihikarukit@gmail.com>
2022-05-24 13:40:09 +0900

    #update ヒープソートのプログラム完成？

--- src/sort_module.f90
+++ src/sort_module.f90
@@ -32,23 +32,28 @@ module sort_mod
         real, intent(out), allocatable :: array_sorted(:)
         real, allocatable :: temp_array(:)  !配列の要素を減らすための一時的な配列
         real parent, child, child_1st, child_2nd
-        integer i, j, node, num_node
+        integer i, j, node, num_node, num_node_heap
 
         num_node = size(array)
-        allocate(array_sorted(numnode))
-
-        if (mod(num_node,2) == 0) then  !要素数の偶奇判定
-            parent = array(num_node/2)
-            child = array(num_node)
-            if(parent < child) then
-                call swap_node(parent, child)
-                array(num_node/2) = parent
-                array(num_node) = child
-            end if
-        end if
-
+        num_node_heap = num_node    !初めは要素全体のノード数とヒープ構造のノード数が同じ
+        allocate(array_sorted(num_node))
         do i = 1, num_node  !ソート後の配列に格納するループ
-            do node = num_node, 3, -2   !ヒープソート一回分のループ
+
+            if (mod(num_node_heap,2) == 0) then  !要素数の偶奇判定(偶数のときだけ特別な処理)
+                parent = array(num_node_heap/2)
+                child = array(num_node_heap)
+
+                if(parent < child) then !要素数が偶数のとき末端のノードだけ2分木にならないのでその処理
+                    call swap_node(parent, child)
+                    array(num_node_heap/2) = parent
+                    array(num_node_heap) = child
+                end if
+
+                num_node_heap = num_node_heap-1   !末端の1分木の分だけ比較に使用するヒープ構造の要素数を減らす
+            
+            end if
+                
+            do node = num_node_heap, 3, -2   !ヒープソート一回分のループ
                 parent = array(int(node/2))
                 child_1st = array(node-1)
                 child_2nd = array(node)
@@ -78,11 +83,52 @@ module sort_mod
 
             deallocate(temp_array)
 
+            num_node_heap = size(array)
+
         end do
+
+        ! else if (mod(num_node,2) == 1) then    !要素数の偶奇判定
+        !     do i = 1, num_node  !ソート後の配列に格納するループ
+        !         do node = num_node, 3, -2   !ヒープソート一回分のループ
+        !             parent = array(int(node/2))
+        !             child_1st = array(node-1)
+        !             child_2nd = array(node)
+        !             child = compare_children(child_1st, child_2nd)
+        !             if(parent < child) then
+        !                 call swap_node(parent, child)
+        !                 array(int(node/2)) = parent
+        !                 if (parent == child_1st) then   !parentは入れ替え後なのでもとはchild
+        !                     array(node-1) = child
+        !                 else
+        !                     array(node) = child
+        !                 end if
+        !             end if
+        !         end do
+    
+        !         array_sorted(i) = array(1)
+        !         allocate(temp_array(num_node-i))
+    
+        !         do j = 2, num_node  !入力配列の最上親ノードを抜き取って1つずらした配列作成
+        !             temp_array(j-1) = array(j)
+        !         end do
+                
+        !         deallocate(array)
+        !         allocate(array(num_node-i))
+    
+        !         array = temp_array
+    
+        !         deallocate(temp_array)
+
+        !         num_node = size(array)
+    
+        !     end do
+        ! end if
+
+
     end subroutine
         
 
-    subroutine heap_sort
-    end subroutine
+    ! subroutine heap_sort
+    ! end subroutine
 
 end module sort_mod
