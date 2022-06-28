82036e3bce89ac5ec5be56d62c0c486272d335b3
Tauzu <tauzushuka@gmail.com>
2022-06-28 15:10:53 +0900

    modify

--- test/sort_test.f90
+++ test/sort_test.f90
@@ -1,24 +1,33 @@
 program sort_test
     use sort_m
+    use array_m
     implicit none
-    integer, parameter :: arraySize = 100
-    type(content_t) array(arraySize), array_sorted(arraySize)
-    integer i
-    real ct1, ct2
+    real, parameter :: test1(10) = [-9.0, -1.0, 0.0, 3.0, 5.0, 7.2, 14.4, 99.9, 122.5, 255.0]
+    real, parameter :: test2(11) = [-99.0, -9.0, -1.0, 0.0, 3.0, 5.0, 7.2, 14.4, 99.9, 122.5, 255.0]
 
-    call random_number(array(:)%value)
-    do i = 1, arraySize
-        array(i)%originID = i
-    end do
+    call testing(test1)
+    print '("==============================================================")'
+    call testing(test2)
 
-    call cpu_time(ct1)
-    call heap_sort(array, array_sorted)
-    call cpu_time(ct2)
+    contains
 
-    do i = 1, arraySize
-        print '(2(i6, x, f20.16, 4x, "|"))', array(i)%originID, array(i)%value, array_sorted(i)%originID, array_sorted(i)%value
-    end do
+    subroutine testing(array_correct)
+        real, intent(in) :: array_correct(:)
+        real tmp(size(array_correct))
+        type(content_t) array(size(array_correct)), array_sorted(size(array_correct))
+        integer i
 
-    print*, ct2 - ct1, 'sec'
+        tmp = FisherYates_shuffle(array_correct)
+        ! print *, tmp
+        array = real2content(tmp)
+        call heap_sort(array, array_sorted)
+
+        if(.not.isEqual(array_sorted(:)%value, array_correct)) error stop
+
+        do i = 1, size(array_sorted)
+            print '(2(i6, x, f20.16, 4x, "|"))', array(i)%originID, array(i)%value, array_sorted(i)%originID, array_sorted(i)%value
+        end do
+
+    end subroutine
     
 end program sort_test
