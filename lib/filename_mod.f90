cde13a20545be4788a6dabc495a8bbbda6843cd2
Tauzu <tauzushuka@gmail.com>
2022-07-24 12:51:30 +0900

    refactoring

--- lib/filename_mod.f90
+++ lib/filename_mod.f90
@@ -1,7 +1,7 @@
 module filename_m
     character(13), parameter :: adjacencyFileName = 'adjacency.txt'
     character(12), parameter :: boundaryFileName = 'boundary.txt'
-    character(21), parameter :: conditionFName = 'condition.nml'
-    character(20), parameter :: IniPositionFName = 'initial_position.csv'
-    character(22), parameter :: IniDistributionFName = 'InitialDistribution.bu'
+    character(21), parameter :: conditionFileName = 'condition.nml'
+    character(20), parameter :: InitialPositionFileName = 'initial_position.csv'
+    character(22), parameter :: InitialDistributionFileName = 'InitialDistribution.bu'
 end module filename_m
