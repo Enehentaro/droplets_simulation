bcd9fafa9f27b53dd608e3dc990a0e0a76d7762c
mochimochikun <konishihikarukit@gmail.com>
2022-09-24 10:33:38 +0900

    謎エラー

--- src/dropletCount.f90
+++ src/dropletCount.f90
@@ -10,10 +10,11 @@ program dropletCount
     use simpleFile_reader
     use path_operator_m
     implicit none
-    integer n, i_box, num_box, caseID, startStep
-    character(255) path2caselist, outputFName
+    integer n, caseID
+    integer k, startSecond, startStep, endStep
+    character(255) path2caselist, outputFNameHead, outputFName, backupFName
     character(50), allocatable :: caseName_array(:)
-    character(:), allocatable :: caseName, fname, path2mainDir
+    character(:), allocatable :: caseName, path2mainDir
     integer, allocatable :: id_array(:)
     type(DropletGroup) mainDroplet, dGroup
     type(conditionValue_t) condVal
@@ -23,106 +24,113 @@ program dropletCount
     double precision min_cdn(3), max_cdn(3)
 
     type boxResult_t
+        type(boxCounter) box
         integer num_droplet
         real volume, RoI
     end type
 
-    type(boxResult_t), allocatable :: results(:)
+    type(boxResult_t), allocatable :: result_overCases(:)
 
     print*, 'path2caselist = ?'
     read(5, '(A)') path2caselist
 
-    print*, 'startStep = ?'
-    read(5, *) startStep
+    print*, 'startSecond = ?'
+    read(5, *) startSecond
 
-    print*, 'outputFName = ?'
-    read(5, '(A)') outputFName
+    print*, 'outputFNameHead = ?'
+    read(5, '(A)') outputFNameHead
 
     call get_DirFromPath(trim(path2caselist), path2mainDir)
     ! call case_check(caseName_array)
     call read_textRecord(trim(path2caselist), caseName_array)
+    allocate(result_overCases(size(caseName_array)))
 
-    allocate(results(size(caseName_array)))
-
+    box_array = get_box_array(path2mainDir // 'box.csv', condVal%num_drop)
     do caseID = 1, size(caseName_array)
-        caseName = path2mainDir // trim(caseName_array(caseID))
-        condVal = read_condition(caseName)
-        deltaTime = real(condVal%dt * condVal%L/condVal%U)
-    
-        box_array = get_box_array(path2mainDir // 'box.csv', condVal%num_drop)
-    
-        num_box = size(box_array)
-    
-        do n = startStep, condVal%stepEnd, condVal%outputInterval
-            if(n==0) then
-                fname = caseName//'/backup/InitialDistribution.bu'
-            else
-                block
-                    character(255) str
-                    write(str,'("'//caseName//'/backup/backup_", i0 , ".bu")') n
-                    fname = trim(str)
-                end block
-            end if
-    
-            mainDroplet = read_backup(fname)
-    
-            do i_box = 1, num_box
-                min_cdn = dble(box_array(i_box)%min_cdn)
-                max_cdn = dble(box_array(i_box)%max_cdn)
+        result_overCases(caseID)%box = box_array(1)
+    end do
+
+    endStep = 600000
+
+    do k = 1, startSecond
+        startStep = startSecond * 10000
+        
+        do caseID = 1, size(caseName_array)
+            caseName = path2mainDir // trim(caseName_array(caseID))
+            condVal = read_condition(caseName)
+            deltaTime = real(condVal%dt * condVal%L/condVal%U)
+        
+            do n = endStep, startStep, -condVal%outputInterval
+                if(n==0) then
+                    backupFName = caseName//'/backup/InitialDistribution.bu'
+                else
+                    write(backupFName,'("'//caseName//'/backup/backup_", i0 , ".bu")') n
+                end if
+                print*, "caseName", caseName
+        
+                mainDroplet = read_backup(trim(backupFName))
+        
+                min_cdn = dble(result_overCases(caseID)%box%min_cdn)
+                max_cdn = dble(result_overCases(caseID)%box%max_cdn)
                 id_array = mainDroplet%IDinBox(min_cdn, max_cdn)
-                call box_array(i_box)%add_Flag(id_array)
+                call result_overCases(caseID)%box%add_Flag(id_array)
+
+                ! if(mod(n, outputInterval) == 0 .and. n /= 0) call calcRoI_and_output
+        
             end do
 
-            ! if(mod(n, outputInterval) == 0 .and. n /= 0) call calcRoI_and_output
-    
+            id_array = result_overCases(caseID)%box%get_FlagID()
+            result_overCases(caseID)%num_droplet = size(id_array)
+            dGroup%droplet = mainDroplet%droplet(id_array)
+            result_overCases(caseID)%volume = real(dGroup%totalVolume() * condVal%L**3 * 1.d6 )    !有次元化[m^3]したのち、[ml]に換算
+
         end do
 
-        id_array = box_array(1)%get_FlagID()
-        results(caseID)%num_droplet = size(id_array)
-        dGroup%droplet = mainDroplet%droplet(id_array)
-        results(caseID)%volume = real(dGroup%totalVolume() * condVal%L**3 * 1.d6 )    !有次元化[m^3]したのち、[ml]に換算
+            
+        write(outputFName, '("'//trim(outputFNameHead)//'_from'//'", i0, "sec.csv")') startSecond
+        call output_CSV_overCases(trim(outputFName))
+
+        endStep = startStep
 
     end do
 
-    call output_CSV_overCases(outputFName)
-
     contains
 
-    subroutine calcRoI_and_output
-        use path_operator_m
-        real erapsedTime
-            !! 経過時間 [ h ]
+    ! subroutine calcRoI_and_output
+    !     use path_operator_m
+    !     real erapsedTime
+    !         !! 経過時間 [ h ]
 
-        type(boxResult_t), allocatable :: bResult(:)
+    !     type(boxResult_t), allocatable :: bResult(:)
 
-        character(255) :: filename, filename2
-        character(:), allocatable :: output_path
+    !     character(255) :: filename, filename2
+    !     character(:), allocatable :: output_path
 
-        erapsedTime = n*deltaTime / 3600.   ! 経過時間 [ h ]
+    !     erapsedTime = n*deltaTime / 3600.   ! 経過時間 [ h ]
         
-        allocate(bResult(num_box))
+    !     allocate(bResult(num_box))
 
-        do i_box = 1, num_box
-            id_array = box_array(i_box)%get_FlagID()
-            dGroup%droplet = mainDroplet%droplet(id_array)
-            bResult(i_box)%num_droplet = size(dGroup%droplet)
-            bResult(i_box)%volume = real(dGroup%totalVolume() * condVal%L**3 * 1.d6 )    !有次元化[m^3]したのち、[ml]に換算
-        end do
+    !     do i_box = 1, num_box
+    !         id_array = result_overCases(caseID)%box%get_FlagID()
+    !         dGroup%droplet = mainDroplet%droplet(id_array)
+    !         bResult(i_box)%num_droplet = size(dGroup%droplet)
+    !         bResult(i_box)%volume = real(dGroup%totalVolume() * condVal%L**3 * 1.d6 )    !有次元化[m^3]したのち、[ml]に換算
+    !     end do
 
-        bResult(:)%RoI = RateOfInfection(bResult(:)%volume, erapsedTime) !1分間あたりの感染確率を計算
+    !     bResult(:)%RoI = RateOfInfection(bResult(:)%volume, erapsedTime) !1分間あたりの感染確率を計算
     
-        output_path = caseName//'/BoxCount_from1sec'
-        call make_directory(output_path)
-        ! call remove_directory(caseName//'/BoxCount')
+    !     output_path = caseName//'/BoxCount_from1sec'
+    !     call make_directory(output_path)
+    !     ! call remove_directory(caseName//'/BoxCount')
 
-        write(filename, '("'//output_path//'/box_", i0, ".csv")') n
-        call output_countCSV(trim(filename), bResult)
+    !     write(filename, '("'//output_path//'/box_", i0, ".csv")') n
+    !     call output_countCSV(trim(filename), bResult)
 
-        write(filename, '("'//output_path//'/Box_", i0, ".vtk")') n
-        write(filename2, '("'//output_path//'/Box_c_", i0, ".vtk")') n
-        call output_boxVTK(trim(filename), trim(filename2), bResult)
+    !     write(filename, '("'//output_path//'/Box_", i0, ".vtk")') n
+    !     write(filename2, '("'//output_path//'/Box_c_", i0, ".vtk")') n
+    !     call output_boxVTK(trim(filename), trim(filename2), bResult)
 
-    end subroutine
+    ! end subroutine
 
     subroutine output_CSV_overCases(csvFName)
         character(*), intent(in) :: csvFName
@@ -154,7 +162,7 @@ program dropletCount
             write(n_unit, '("casename,num_drop,volume[ml]")')
             
             do i = 1, size(caseName_array)
-                write(n_unit,'(*(g0:,","))') trim(caseName_array(i)), results(i)%num_droplet, results(i)%volume
+                write(n_unit,'(*(g0:,","))') trim(caseName_array(i)), result_overCases(i)%num_droplet, result_overCases(i)%volume
             end do
 
         close(n_unit)
@@ -180,42 +188,42 @@ program dropletCount
 
     end subroutine
 
-    subroutine output_boxVTK(vtkFName, vtkFName2, bResult)
-        use VTK_operator_m
-        character(*), intent(in) :: vtkFName
-        character(*), intent(in) :: vtkFName2
-        type(boxResult_t), intent(in) :: bResult(:)
-        type(UnstructuredGrid_inVTK) mesh
-        integer i, j, k
-        real, parameter :: trans(3,8) = reshape([ &
-                                            0.0,0.0,0.0, 1.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0, &
-                                            0.0,0.0,1.0, 1.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0], shape(trans))
+    ! subroutine output_boxVTK(vtkFName, vtkFName2, bResult)
+    !     use VTK_operator_m
+    !     character(*), intent(in) :: vtkFName
+    !     character(*), intent(in) :: vtkFName2
+    !     type(boxResult_t), intent(in) :: bResult(:)
+    !     type(UnstructuredGrid_inVTK) mesh
+    !     integer i, j, k
+    !     real, parameter :: trans(3,8) = reshape([ &
+    !                                         0.0,0.0,0.0, 1.0,0.0,0.0, 0.0,1.0,0.0, 1.0,1.0,0.0, &
+    !                                         0.0,0.0,1.0, 1.0,0.0,1.0, 0.0,1.0,1.0, 1.0,1.0,1.0], shape(trans))
                                         
-        real, allocatable :: xyz(:,:), num_droplet(:)
-        integer, allocatable :: vertices(:,:), types(:)
+    !     real, allocatable :: xyz(:,:), num_droplet(:)
+    !     integer, allocatable :: vertices(:,:), types(:)
                                         
-        allocate(xyz(3, num_box*8))
-        allocate(vertices(8, num_box), types(num_box))
-        do i = 1, num_box
+    !     allocate(xyz(3, num_box*8))
+    !     allocate(vertices(8, num_box), types(num_box))
+    !     do i = 1, num_box
 
-            do j = 1, 8
-                k = j + 8*(i-1)
-                xyz(:,k) = box_array(i)%min_cdn(:) + box_array(i)%width(:)*trans(:,j)
-                vertices(j,i) = k
-            end do
+    !         do j = 1, 8
+    !             k = j + 8*(i-1)
+    !             xyz(:,k) = box_array(i)%min_cdn(:) + box_array(i)%width(:)*trans(:,j)
+    !             vertices(j,i) = k
+    !         end do
 
-            types(i) = 11
+    !         types(i) = 11
 
-        end do
+    !     end do
 
-        mesh = UnstructuredGrid_inVTK_(xyz, vertices, types)
+    !     mesh = UnstructuredGrid_inVTK_(xyz, vertices, types)
 
-        call mesh%output(vtkFName, cellScalar=bResult(:)%RoI, scalarName='RoI')
+    !     call mesh%output(vtkFName, cellScalar=bResult(:)%RoI, scalarName='RoI')
 
-        num_droplet = real(bResult(:)%num_droplet)
-        call mesh%output(vtkFName2, cellScalar=num_droplet, scalarName='Droplets')
+    !     num_droplet = real(bResult(:)%num_droplet)
+    !     call mesh%output(vtkFName2, cellScalar=num_droplet, scalarName='Droplets')
 
-    end subroutine
+    ! end subroutine
 
     
     elemental real function RateOfInfection(volume, erapsedTime)
