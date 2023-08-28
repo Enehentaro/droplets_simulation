64e1e9b9ba530206975076f604c6dd3437fe9019
Nishimura837 <ytn602272@icloud.com>
2023-08-28 15:33:42 +0900

    test

--- src/dropletCount.f90
+++ src/dropletCount.f90
@@ -8,7 +8,7 @@ program dropletCount
     use boxCounter_m
     use caseName_m
     implicit none
-    integer n, i_box, num_box, caseID
+    integer n, i_box, num_box, caseID , i 
     character(50), allocatable :: caseName_array(:)
     character(:), allocatable :: caseName, fname
     integer, allocatable :: id_array(:)
@@ -20,6 +20,7 @@ program dropletCount
     type boxResult_t
         integer num_droplet
         real volume, RoI
+        integer num_adhes
     end type
     type(boxResult_t), allocatable :: bResult(:)
 
@@ -27,35 +28,46 @@ program dropletCount
     !print*, 'caseName = ?'
     !read(5, *) caseName
 
-    do caseID = 1, size(caseName_array)
-        caseName = trim(caseName_array(caseID))
+    do caseID = 1, size(caseName_array)                     
+            !ケースの数だけ繰り返す。複数の場合はテキストファイルに出力しておく必要がある
+        caseName = trim(caseName_array(caseID))             
+            !caseName_arrayにある名前には余分な空白があるからtrimで文字の部分だけを取り出している　
         condVal = read_condition(caseName)
         ! baseParam = BasicParameter_(condVal%dt, condVal%L, condVal%U)
     
         box_array = get_box_array(caseName, condVal%num_drop)
     
-        num_box = size(box_array)
+        num_box = size(box_array)   
+            !データのサイズだから今であればboxの数
     
-        do n = 0, condVal%stepEnd, condVal%outputInterval
-            if(n==0) then
-                fname = caseName//'/backup/InitialDistribution.bu'
-            else
-                block
-                    character(255) str
-                    write(str,'("'//caseName//'/backup/backup_", i0 , ".bu")') n
-                    fname = trim(str)
-                end block
-            end if
+        ! do n = 0, condVal%stepEnd, condVal%outputInterval
+        !     if(n==0) then
+        !         fname = caseName//'/backup/InitialDistribution.bu'
+        !     else
+        !         block
+        !             character(255) str
+        !             write(str,'("'//caseName//'/backup/backup_", i0 , ".bu")') n
+        !             fname = trim(str)
+        !         end block
+        !     end if
     
-            mainDroplets = read_backup(fname)
+        !     mainDroplets = read_backup(fname)
     
+        !     do i_box = 1, num_box
+        !         id_array = dropletIDinBox(mainDroplets, dble(box_array(i_box)%min_cdn), dble(box_array(i_box)%max_cdn))
+        !         call box_array(i_box)%add_Flag(id_array)
+        !     end do
+    
+        ! end do
+
+        fname = caseName//'/backup/backup_10000.bu'
+        mainDroplets = read_backup(fname)
             do i_box = 1, num_box
                 id_array = dropletIDinBox(mainDroplets, dble(box_array(i_box)%min_cdn), dble(box_array(i_box)%max_cdn))
                 call box_array(i_box)%add_Flag(id_array)
             end do
-    
-        end do
-    
+            !浮遊している飛沫にフラグを立てる
+
         allocate(bResult(num_box))
 
         do i_box = 1, num_box
@@ -63,6 +75,13 @@ program dropletCount
             droplets = mainDroplets(id_array)
             bResult(i_box)%num_droplet = size(droplets)
             bResult(i_box)%volume = real(dropletTotalVolume(droplets) *condVal%L**3 * 1.d6 )    !有次元化[m^3]したのち、[ml]に換算
+            bResult(i_box)%num_adhes = 0
+            do i = 1 , size(droplets)
+                if(.not.droplets(i)%isFloating()) then
+                    bResult(i_box)%num_adhes = bResult(i_box)%num_adhes + 1 
+                endif
+            enddo
+            !adhes は付着した飛沫の数であるから、その総数を計算している
         end do
 
         bResult(:)%RoI = RateOfInfection(bResult(:)%volume) !1分間あたりの感染確率を計算
@@ -85,10 +104,11 @@ program dropletCount
 
         open(newunit=n_unit, file=csvFName, status='replace')
         
-            write(n_unit, '("x,y,z,num_drop,volume[ml],RoI")')
+            write(n_unit, '("x,y,z,num_drop,volume[ml],RoI,num_adhes")')
             
             do i = 1, size(box_array)
-                write(n_unit,'(*(g0:,","))') box_array(i)%center, bResult(i)%num_droplet, bResult(i)%volume, bResult(i)%RoI
+                write(n_unit,'(*(g0:,","))') box_array(i)%center, bResult(i)%num_droplet,&
+                bResult(i)%volume, bResult(i)%RoI,bResult(i)%num_adhes
             end do
 
         close(n_unit)
